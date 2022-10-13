7fdbea7f4217b618bb210772d98d69aa8f4bdf44
learnthamby <113592841+learnthamby@users.noreply.github.com>
2022-10-13 22:36:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var startimage = 1; //choose the image for startup
-var sliderduration = 2000;
-var animationEffects = "fadeIn";
+var sliderduration = 2000; //choose the time duration
+var animationEffects = "fadeIn"; ////choose the animation from https://animate.style/ (https://github.com/animate-css/animate.css)
+
 var get_allimageLength = $(".sliderHolder ul li").length;
 $getallImages = $(".sliderHolder ul li");
 var slideradd = 0;
